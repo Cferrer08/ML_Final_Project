4154246f48152465f22678a504af29f83bf76502
Donovan <dminer1@fordham.edu>
2024-12-08 22:56:40 -0500

    ROCAUC graphs

--- thirdTry.py
+++ thirdTry.py
@@ -2,25 +2,24 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import RocCurveDisplay
 import time
+import scikitplot as skplt
+import matplotlib.pyplot as plt
+
+train_path = './SPECTF_train.csv'
+test_path = './SPECTF_test.csv'
+
+train_data = pd.read_csv(train_path, header=None)
+test_data = pd.read_csv(test_path, header=None)
 
 
-train_path = './SPECTFtrain.csv'
-test_path = './SPECTFtest.csv'
+X_train = train_data.iloc[0:80, 1:] 
+y_train = train_data.iloc[0:80, 0]
 
-train_data = pd.read_csv(train_path)
-test_data = pd.read_csv(test_path)
+X_test = test_data.iloc[0:187, 1:]
+y_test = test_data.iloc[0:187, 0]
 
-
-test_data.columns = train_data.columns
-
-X_train = train_data.iloc[:, 1:] 
-y_train = train_data.iloc[:, 0]
-
-X_test = test_data.iloc[:, 1:]
-y_test = test_data.iloc[:, 0]
+print(f'{X_train}\n{y_train}\n{X_test}\n{y_test}')
 
 # Normalize the feature data using StandardScaler
 scaler = StandardScaler()
@@ -44,14 +43,19 @@ for kernel in kernels:
         start_time = time.time()
         
         
-        model = SVC(kernel=kernel, C=C, degree=2 if kernel == "poly" else 3)  
+        model = SVC(kernel=kernel, C=C, degree=2 if kernel == "poly" else 3, probability=True)  
         model.fit(X_train_scaled, y_train)
         
         y_pred = model.predict(X_test_scaled)
-                
+        class_prob = model.predict_proba(X_test_scaled)        
+
         accuracy = accuracy_score(y_test, y_pred)
         runtime = time.time() - start_time
         results.append((kernel, C, accuracy, runtime))
+
+        skplt.metrics.plot_roc(y_test, class_prob)
+        plt.title(f'SVM, {kernel}, C - {C}')
+        plt.show()
 
 results_df = pd.DataFrame(results, columns=["Kernel", "C", "Accuracy", "Runtime (s)"])
 results_df_sorted = results_df.sort_values(by=["Kernel", "C"], ascending=True).reset_index(drop=True)
@@ -59,4 +63,3 @@ results_df_sorted = results_df.sort_values(by=["Kernel", "C"], ascending=True).r
 # Print the results DataFrame
 print("SVM Results with Different Kernels and Slack Variable (C):")
 print(results_df_sorted)
-
